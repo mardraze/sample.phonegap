dcf81572365ff69fed2bdf44cdeca24c52828717
Marcin Drazek <marcindr1@gmail.com>
2014-09-12 22:24:07 +0200

    install.bat

--- install_all.bat
+++ install_all.bat
@@ -1,6 +1,32 @@
+IF "%1" NEQ "" (
+	goto start
+	
+)
+echo "PLEASE ENTER PROJECT NAME PARAM"
+pause
+exit
+
+:start
+
 WHERE npm
-IF %ERRORLEVEL% NEQ 0 echo "PLEASE DOWNLOAD NODEJS http://nodejs.org/download/"
+IF %ERRORLEVEL% NEQ 0 (
+	echo "PLEASE DOWNLOAD NODEJS http://nodejs.org/download/" 
+	pause
+	exit
+)
+WHERE cordova
+IF %ERRORLEVEL% NEQ 0 npm install cordova
+
+call cordova create %*
+cd %1/www
+
 WHERE bower
 IF %ERRORLEVEL% NEQ 0 npm install bower
-bower install mardraze/sample.phonegap
+call bower install mardraze/sample.phonegap
 
+rd /S /Q index.old.html
+move index.html index.old.html
+
+copy bower_components\sample.phonegap\index.html .
+
+pause
